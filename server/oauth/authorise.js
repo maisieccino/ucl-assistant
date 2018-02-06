adb76d05ebda09a4a14415c5ea171e86fea6605f
Matthew Bell <matt@mbell.me>
2018-02-06 14:59:53 +0000

    Temporarily use staging.ninja

--- server/oauth/authorise.js
+++ server/oauth/authorise.js
@@ -1,11 +1,12 @@
 const moment = require("moment");
+const { API_URL } = require("../uclapi/constants.js");
 
 module.exports = async ctx => {
   ctx.session = {
     state: moment().valueOf(),
     redirectURL: decodeURIComponent(ctx.query.return) || "UCLAssistant://+auth",
   };
-  const url = `https://uclapi.com/oauth/authorise?client_id=${
+  const url = `${API_URL}/oauth/authorise?client_id=${
     process.env.UCLAPI_CLIENT_ID
   }&state=${ctx.session.state}`;
   ctx.redirect(url);
